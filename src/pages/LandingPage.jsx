6e0c5bbb5548ab5afb8736718bd0702f88c95b7f
Victoria B. Amponsah <victoriaboatemaa737@gmail.com>
2025-08-14 23:27:34 +0000

    LandingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import assets from "../assets/assests.jsx";
 
 const LandingPage = () => {
-  return (
-    <div>
+  const images = [assets.bg5, assets.bg2, assets.bg3, assets.bg5];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-      {/* LandingPage */}
-      <img className="w-screen h-[1024px]"
-      src={assets.bg4} alt="" />
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [images.length]);
+
+  return (
+    <div className="w-full">
+      {/* Hero Section */}
+      <section className="flex flex-col sm:flex-row items-center">
+        <img
+          className="w-full sm:w-1/2 h-[60vh] sm:h-[1024px] object-cover"
+          src={images[currentImageIndex]}
+          alt="Animation background"
+        />
+      </section>
+
+      {/* Discovery Section */}
+      <section className="bg-[#e9eaef] font-semibold w-full flex items-center justify-between px-2 sm:px-4">
+        {/* Left Side */}
+        <h2 className="text-xs sm:text-sm md:text-base whitespace-nowrap mr-3">
+          DISCOVER THE EXPERIENCE
+        </h2>
+
+        {/* Right Side */}
+        <ul className="flex items-center bg-[#1E305E] text-white text-[10px] sm:text-xs md:text-sm whitespace-nowrap overflow-x-auto ">
+          <li className="p-2 sm:p-3 md:p-4 hover:bg-blue-900 cursor-pointer">
+            HOW TRUST IS BUILT
+          </li>
+          <li className="border-l border-white p-2 sm:p-3 md:p-4 hover:bg-blue-900 cursor-pointer">
+            ABOUT US
+          </li>
+          <li className="border-l border-white p-2 sm:p-3 md:p-4 hover:bg-blue-900 cursor-pointer">
+            OUR PROCESSES
+          </li>
+          <li className="border-l border-white p-2 sm:p-3 md:p-4 hover:bg-blue-900 cursor-pointer">
+            CUSTOMER CARE
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
